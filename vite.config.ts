4ab70ae0948be9fdafd60782e14e25e566cd1ea6
yashchaud <yashchaudhari908@gmail.com>
2025-05-12 15:35:07 +0530

    das

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,14 +10,14 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },  
+  },
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "http://15.207.30.113",
 
         changeOrigin: true,
       },
     },
-  }
-})
+  },
+});
